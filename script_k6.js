e82ec17e8925fe8405ead1a4b66b3bd28e3d04e5
walkerpig <walkerpig@gmail.com>
2023-08-21 16:38:27 +0700

    add thresshold

--- script_k6.js
+++ script_k6.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { check, sleep } from "k6";
+import { check, sleep } from 'k6';
 
 const isNumeric = (value) => /^\d+$/.test(value);
 
@@ -10,30 +10,29 @@ const target_vus_env = `${__ENV.TARGET_VUS}`;
 const target_time_env = `${__ENV.TARGET_TIME}`;
 const target_vus = isNumeric(target_vus_env) ? Number(target_vus_env) : default_vus;
 const target_time = isNumeric(target_time_env) ? Number(target_time_env) : default_times;
-const target_time_in_minutes = target_time + "m";
-
-export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
-  },
-};
+const target_time_in_minutes = target_time + 'm';
 
 export let options = {
+  thresholds: {
+    // Đặt ngưỡng cho tỷ lệ lỗi http_req_failed (<0.01%)
+    http_req_failed: ['rate<0.01'],
+    // Đặt ngưỡng cho thời gian trung bình của http_req_duration (p(95)<200ms)
+    //http_req_duration: ['p(95)<200'],
+  },
   stages: [
-      // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 5s
-      { duration: target_time_in_minutes, target: target_vus },
+    // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 5s
+    { duration: target_time_in_minutes, target: target_vus },
 
-      // Stay at rest on TARGET_VUS VUs for 10s
-      { duration: target_time_in_minutes, target: target_vus },
+    // Stay at rest on TARGET_VUS VUs for 10s
+    { duration: target_time_in_minutes, target: target_vus },
 
-      // Ramp-down from TARGET_VUS to 0 VUs for 5s
-      { duration: "1m", target: 0 }
-  ]
+    // Ramp-down from TARGET_VUS to 0 VUs for 5s
+    { duration: '1m', target: 0 },
+  ],
 };
 
 export default function () {
   const response = http.get('https://registry.aws.platform.vpbank.dev/repository/maven-proxy-group/classworlds/classworlds/1.1/classworlds-1.1.jar');
-  check(response, { "status is 200": (r) => r.status === 200 });
+  check(response, { 'status is 200': (r) => r.status === 200 });
   //sleep(.100);
-};
+}
